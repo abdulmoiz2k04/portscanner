72e7a65fec5c9defa9e6a8a571f46f63c2411d7d
Abdul Moiz <abdulmoiz2k04@gmail.com>
2024-11-12 14:26:29 +0500

    added file for vercel

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.getElementById('scanForm').addEventListener('submit', async (event) => 
     resultBox.textContent = 'Scanning...';
 
     try {
-        const response = await fetch('http://127.0.0.1:5000/scan', {
+        const response = await fetch('https://portscanner.vercel.app/scan', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
